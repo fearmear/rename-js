536caa90d8f8f9fd090c481c810c8cc0f78865e1
Dmitry Kozlovsky <dkozlovsky@fitbit.com>
2016-05-24 20:17:56 +0300

    optional context directory support

--- rename.js
+++ rename.js
@@ -6,6 +6,7 @@ var fileMode, renameProcessorScript
 
 var from = process.argv[2]
 var to = process.argv[3]
+var cwd = process.argv[4] ? _path.resolve(process.argv[4]) : process.cwd();
 
 if(from === undefined || to === undefined){
 	console.log('[ERROR] arguments missed. see [https://github.com/peichao01/rename-js] for more infomation.')
@@ -23,13 +24,17 @@ else{
 	console.log('files need match RegExp: ' + regFrom)
 }
 
-var walker = walk.walk(process.cwd(), {})
+if (process.argv[4]) {
+	console.log('context directory was set to ', cwd);
+}
+
+var walker = walk.walk(cwd, {})
 
 walker.on('node', function(root, fileStats, next){
-	var filename = _path.relative(process.cwd(), _path.join(root, fileStats.name))
+	var filename = _path.relative(cwd, _path.join(root, fileStats.name))
 
 	if(fileMode){
-		var processor = require(_path.join(process.cwd(),  renameProcessorScript))
+		var processor = require(_path.join(cwd,  renameProcessorScript))
 		var toName = processor(filename)
 		if(typeof toName == 'string' && filename !== toName){
 			rename(filename, toName, next)
@@ -55,7 +60,7 @@ walker.on('end', function(){
 
 function rename(filename, toName, next){
 	console.log('renaming ['+filename+'] to ['+toName+'].')
-	fs.rename(filename, toName, function(err){
+	fs.rename(_path.join(cwd, filename), _path.join(cwd, toName), function(err){
 		if(err) throw err
 		next()
 	})
